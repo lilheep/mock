e45b1c85a543e1329d83670362193721c355ec1b
lilheep <andrey.arkhipov.2006@mail.ru>
2026-03-27 08:41:46 +0300

    update: mini

--- src/main/java/com/prmo/mock/config/SwaggerConfig.java
+++ src/main/java/com/prmo/mock/config/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package com.prmo.mock.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,10 +14,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Pre-Trip Medical Check API")
                         .description("API for pre-trip medical examinations of drivers and doctors")
-                        .version("1.0")
-                        .contact(new Contact()
-                                .email("nmirnaa607@gmail.com")
-                                .name("lilheep")));
+                        .version("1.0"));
     }
 
 }
